186d495cba8dab563074b683ecdb15417ca7984a
SamOstlund <ostlundsam@gmail.com>
2019-01-24 17:32:26 -0500

    Updated WeaponFactory.cpp
    Added new weapons to the factory.
    
    Edited the getWeapon function so that a hammer, crazy sword, and a rare sword can be created.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -1,14 +1,17 @@
-/* 
+/*
  * File:   WeaponFactory.cpp
  * Author: Javier <jrescobara@gmail.com>
- *  
- */ 
+ * Additions: Sam Ostlund
+ */
 
 #include <string>
-#include <stddef.h> 
+#include <stddef.h>
 #include "WeaponFactory.h"
-#include "CommonSword.h" 
+#include "CommonSword.h"
 #include "CommonSpear.h"
+#include "SimpleHammer.h"
+#include "CrazyRandomSword.h"
+#include "RareSword.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -23,9 +26,21 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
+
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("hammer") == 0) {
+      return new SimpleHammer();
+    }
+
+    if (name.compare("crazy") == 0) {
+      return new CrazyRandomSword();
+    }
+
+    if (name.compare("rare") == 0) {
+      return new RareSword();
     }
 
     throw "Invalid weapon";
