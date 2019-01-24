af6428ac2a71659a9126aa47d6af345ea0ae60ee
SamOstlund <ostlundsam@gmail.com>
2019-01-24 16:33:49 -0500

    Updated SimpleHammer.h
    Finished the header file for the Simple Hammer class. I inherited from the weapon class.

--- SimpleHammer.h
+++ SimpleHammer.h
@@ -4,3 +4,26 @@
  *
  * Created on January 24th, 2019
  */
+
+ #include <string>
+ #include "Weapon.h"
+
+ #ifndef SIMPLEHAMMER_H
+ #define SIMPLEHAMMER_H
+
+ /**
+  * Defines the behavior of a simple hammer (hitpoint = 25, ignores all armor if armor is below 30)
+  */
+
+  class SimpleHammer : public Weapon {
+  public:
+
+        SimpleHammer() : Weapon("Simple hammer", 25.0) { //Calls Weapon(name, hitpoints) constructor with values simple hammer and 25.0
+        }
+
+        virtual ~SimpleHammer() {};
+
+        virtual double hit(double armor);
+
+  };
+ #endif /* SIMPLEHAMMER_H */
