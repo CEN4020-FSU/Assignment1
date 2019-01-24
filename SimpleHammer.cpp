f02b0156c84992137ac5daaf698df738a9c879f3
SamOstlund <ostlundsam@gmail.com>
2019-01-24 16:38:03 -0500

    Created SimpleHammer.cpp
    The SimpleHammer.cpp file has been created to reflect the amount of damage done by the simple hammer. It also takes in to account whether the damage taker has 30 armor or less.

---
+++ SimpleHammer.cpp
@@ -0,0 +1,18 @@
+/*
+ * File:   SimpleHammer.cpp
+ * Author: Sam Ostlund (sco16c@my.fsu.edu)
+ *
+ * Created on January 24th, 2019
+ */
+
+#include "SimpleHammer.h"
+
+double SimpleHammer::hit(double armor)
+ {
+    if (armor < 30) //checks if the armor is less than 30, if so then it just does 25 damage.
+      return 25;
+    double damage = hitPoints - (armor * 0.8); //actual damage given they have 30 or more armor.
+    if (damage < 0)
+        return 0;
+    return damage;
+}
