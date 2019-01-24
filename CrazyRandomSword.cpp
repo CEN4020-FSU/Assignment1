aec3545f34b410fc37c33459a9b997a95c593bd8
SamOstlund <ostlundsam@gmail.com>
2019-01-24 17:16:16 -0500

    CrazyRandomSword created
    The header file and the function file of CrazyRandomSword have been created and should be usable in the game. The sword does random damage between 7 and 100 and ignores 2 to a third of the armor of the user getting hit.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,23 @@
+/*
+ * File:   CrazyRandomSword.cpp
+ * Author: Sam Ostlund (sco16c@my.fsu.edu)
+ *
+ * Created on January 24th, 2019
+ */
+
+ #include  "CrazyRandomSword.h"
+ #include <math>
+
+ double CrazyRandomSword::hit(double armor)
+ {
+     srand(time(NULL)); //seeds the RNG
+     double third = floor(armor / 3); //finds a third of the armor and floords it
+     double range = third - 2; //finds the range between the third of armor and 2
+     double reducedArmor = rand() % range + 2; //finds a random number generator between 2 and the third of armor
+     armor = armor - reducedArmor; //reduces the armor since it is being ignored
+     double damage = hitPoints - (armor * 0.8); //
+     if (damage < 0) {
+         return 0;
+     }
+     return damage;
+ }
---
+++ CrazyRandomSword.h
@@ -0,0 +1,33 @@
+/*
+ * File:   CrazyRandomSword.h
+ * Author: Sam Ostlund (sco16c@my.fsu.edu)
+ *
+ * Created on January 24th, 2019
+ */
+
+ #include <string>
+ #include <time>
+ #include <stdlib>
+ #include "Weapon.h"
+
+ #ifndef CRAZYRANDOMSWORD_H
+ #define CRAZYRANDOMSWORD_H
+
+ /**
+  * Defines the behavior of a crazy random sword (hitpoint = random integer between 7 and 100, ignores a random amount of armor from 2 to a third of the armor the weapon hits)
+  */
+
+  srand(time(NULL));
+  class CrazyRandomSword : public Weapon {
+
+  public:
+
+        CrazyRandomSword() : Weapon("Crazy random sword", rand() % 93 + 7) { //Calls Weapon(name, hitpoints) constructor with values crazy random sword and 25.0
+        }
+
+        virtual ~SimpleHammer() {};
+
+        virtual double hit(double armor);
+
+  };
+ #endif /* CRAZYRANDOMSWORD_H */
