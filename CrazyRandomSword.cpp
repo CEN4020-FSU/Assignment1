af96fae8eec27c49ef09a733b7f44f55f0aa3962
SamOstlund <ostlundsam@gmail.com>
2019-01-24 17:50:05 -0500

    Updated CrazyRandomSword.cpp
    Changed the way the reduced armor is calculated.
    
    Previously had a double being used instead of an integer which caused issues with the rand() function. I have instead used a static cast to make it an int.

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -13,7 +13,8 @@
      srand(time(NULL)); //seeds the RNG
      double third = floor(armor / 3); //finds a third of the armor and floords it
      double range = third - 2; //finds the range between the third of armor and 2
-     double reducedArmor = rand() % range + 2; //finds a random number generator between 2 and the third of armor
+     int irange = static_cast<int>(range);
+     double reducedArmor = rand() % irange + 2; //finds a random number generator between 2 and the third of armor
      armor = armor - reducedArmor; //reduces the armor since it is being ignored
      double damage = hitPoints - armor; //
      if (damage < 0) {
