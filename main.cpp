fcbf24ae6294067d135bf6c317a7dd9836a8e28f
eea17b <37185924+eea17b@users.noreply.github.com>
2019-02-03 13:14:37 -0500

    Revert "Main method changed"
    This reverts commit 3d373c9306da94b5591fc623b72f5be12fe0f447.

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ void simulateWeapon(Weapon * weapon, double armor) {
  */
 int main(int argc, char** argv) {
 
-    double armor = 29;
+    double armor = 20;
 
     Weapon *weapon = WeaponFactory::getInstance()->getWeapon("sword");
     simulateWeapon(weapon, armor);
